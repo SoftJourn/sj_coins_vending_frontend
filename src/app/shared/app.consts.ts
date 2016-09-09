40a07d2ef0f6f87d1966fe916521d214693b1de0
vkraietskyi <vkraietskyi@softjourn.com>
2016-09-09 20:49:09 +0300

    Added screens to products and category menus

--- src/app/shared/app.consts.ts
+++ src/app/shared/app.consts.ts
@@ -3,8 +3,8 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AppConsts {
-  API_VENDING_ENDPOINT = 'https://localhost:8222/v1';
-  AUTH_ENDPOINT = 'https://localhost:8111/oauth/token';
-  USER_ENDPOINT = 'https://localhost:8111/api/v1/users';
+  API_VENDING_ENDPOINT = 'https://sjcoins.testing.softjourn.if.ua/vending/v1';
+  AUTH_ENDPOINT = 'https://sjcoins.testing.softjourn.if.ua/auth/oauth/token';
+  USER_ENDPOINT = 'https://sjcoins.testing.softjourn.if.ua/auth/api/v1/users';
   CLIENT_AUTH_HASH = 'dXNlcl9jcmVkOnN1cGVyc2VjcmV0';
 }
---
+++ src/app/shared/services/dashboard.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, Inject } from '@angular/core';
+import { AppConsts } from "../app.consts";
+import { Http, Headers } from "@angular/http";
+import { Observable } from "rxjs/Rx";
+import { AppError } from "../app-error";
+
+@Injectable()
+export class DashboardService {
+
+  constructor() { }
+
+}
