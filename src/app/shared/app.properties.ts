6d2270f9b779b86e71f309a8f79dff6decfd613d
ovovchuk <ovovchuk@softjourn.com>
2016-10-12 17:50:02 +0300

    Fixed ripple bug in login button.

--- src/app/shared/app.properties.ts
+++ src/app/shared/app.properties.ts
@@ -1,5 +1,5 @@
 export class AppProperties {
-  static API_VENDING_ENDPOINT = 'https://localhost:8222/v1';
+  static API_VENDING_ENDPOINT = 'https://sjcoins.testing.softjourn.if.ua/vending/v1';
   static AUTH_SERVER='https://sjcoins.testing.softjourn.if.ua/auth';
   static AUTH_ENDPOINT = AppProperties.AUTH_SERVER +'/oauth/token';
   static AUTH_API = AppProperties.AUTH_SERVER + '/api/v1';
