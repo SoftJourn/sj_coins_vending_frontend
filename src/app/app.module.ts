8163e3f808b773b02aeeae90507228f1b6abdf65
ovovchuk <ovovchuk@softjourn.com>
2016-10-13 17:40:02 +0300

    Added missed dependency.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule,FormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 import { AppComponent } from "./app.component";
 import { routing, appRoutingProviders } from "./app.routing";
@@ -24,7 +24,7 @@ import { CategoryService } from "./shared/services/category.service";
 import { TokenService } from "./shared/services/token.service";
 import { AccountService } from "./shared/services/account.service";
 import { HttpService } from "./shared/services/http.service";
-import { UsersComponent} from "./users/users.component";
+import { UsersComponent } from "./users/users.component";
 import { MdCardModule } from "@angular/material/card";
 import { MachineSizePipe } from "./machines/machine-item/machine-size.pipe";
 import { ProductService } from "./shared/services/product.service";
@@ -34,9 +34,9 @@ import { AdminUsersService } from "./shared/services/admin.users.service";
 import { EditProductComponent } from './products/edit-product/edit-product.component';
 import { ImageUploadService } from './shared/services/image-upload.service';
 import { PurchasesComponent } from './purchases/purchases.component';
-import {PurchaseService} from "./shared/services/purchase.service";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {AddUserComponent} from "./users/add-user/add-user.component";
+import { PurchaseService } from "./shared/services/purchase.service";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AddUserComponent } from "./users/add-user/add-user.component";
 
 @NgModule({
   declarations: [
