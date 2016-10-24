39ef93797f797da102ee9e1356d4f2509d216c2c
ovovchuk <ovovchuk@softjourn.com>
2016-10-24 17:32:43 +0300

    Migration to new host.

--- src/app/shared/app.properties.ts
+++ src/app/shared/app.properties.ts
@@ -1,6 +1,6 @@
 export class AppProperties {
-  static API_VENDING_ENDPOINT = 'https://sjcoins.testing.softjourn.if.ua/vending/v1';
-  static AUTH_SERVER='https://sjcoins.testing.softjourn.if.ua/auth';
+  static API_VENDING_ENDPOINT = 'https://sjcoins-testing.softjourn.if.ua/vending/v1';
+  static AUTH_SERVER='https://sjcoins-testing.softjourn.if.ua/auth';
   static AUTH_ENDPOINT = AppProperties.AUTH_SERVER +'/oauth/token';
   static AUTH_API = AppProperties.AUTH_SERVER + '/api/v1';
   static CLIENT_AUTH_HASH = 'dXNlcl9jcmVkOnN1cGVyc2VjcmV0';
